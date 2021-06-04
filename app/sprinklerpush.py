c7e4222d1a8be18a6ea173b10c6704eb397ac979
Christoph Eßer <ce@ehser-office-group.de>
2021-06-04 13:38:59 +0200

    * exchange print commands by real logging * added status check for running stations * changed log messages to refer to end of run instead of start * reformatted push messages

--- app/sprinklerpush.py
+++ app/sprinklerpush.py
@@ -2,11 +2,17 @@ import requests
 import json
 import redis
 import pause
+import logging
 from datetime import datetime, timedelta, date, time
 from pathlib import Path
+from funcy import omit
+
+logging.basicConfig(level=logging.DEBUG)
+
 # ospri_base = 'http://192.168.29.137'
 
 # read and parse config file
+# p = Path(__file__).parent.joinpath('..', 'config', 'config.json')
 p = Path(__file__).parent.joinpath('config', 'config.json')
 with p.open('r') as myfile:
   data=myfile.read()
@@ -27,14 +33,28 @@ def pushMessage(title, message, timestamp=datetime.now().timestamp()):
   requests.post(config['pushover']['uri'], data)
 
 def waterLevelMessage(water_level):
-  title = 'OpenSprinkler'
+  title = 'OpenSprinkler - Neuer Water Level'
   message = f'Aktueller Water Level für <a href="{config["opensprinkler"]["base_uri"]}">{config["opensprinkler"]["name"]}</a>: <b>{water_level}%</b>'
   pushMessage(title, message)
 
+def statusMessage(mapped_status, water_level):
+  title = 'OpenSprinkler - Bewässerung gestartet'
+
+  message = f'''Bewässerung für <a href="{config["opensprinkler"]["base_uri"]}">{config["opensprinkler"]["name"]}</a>:
+Programm <b>{mapped_status["status"]["program"]}</b> startete Kreis <b>{mapped_status["station"]}</b> um <b>{mapped_status["status"]["start"].strftime("%H:%M:%S")}</b>.
+Bewässerungszeit <b>{str(mapped_status["status"]["duration"])}</b> (WL {water_level}%).'''
+
+  pushMessage(title, message, mapped_status["status"]["start"].timestamp())
+
 def logMessage(mapped_log, water_level):
-  title = 'OpenSprinkler'
-  message = f'Bewässerung für <a href="{config["opensprinkler"]["base_uri"]}">{config["opensprinkler"]["name"]}</a>: \nProgramm <b>{mapped_log["program"]}</b> startete Kreis <b>{mapped_log["station"]}</b> um <b>{mapped_log["start"].strftime("%H:%M:%S")}</b> für <b>{str(mapped_log["duration"])}.</b>\nWL {water_level}%'
-  pushMessage(title, message, mapped_log["start"].timestamp())
+  title = 'OpenSprinkler - Bewässerung gestoppt'
+
+  message = f'''Bewässerung für <a href="{config["opensprinkler"]["base_uri"]}">{config["opensprinkler"]["name"]}</a>:
+Programm <b>{mapped_log["program"]}</b> stoppte Kreis <b>{mapped_log["station"]}</b> um <b>{mapped_log["end"].strftime("%H:%M:%S")}</b>.
+Bewässerungszeit <b>{str(mapped_log["duration"])}</b> (WL {water_level}%).'''
+
+  pushMessage(title, message, mapped_log["end"].timestamp())
+
 
 def json_serial(obj):
   """JSON serializer for objects not serializable by default json code"""
@@ -49,7 +69,7 @@ cache = redis.Redis(host=config['redis']['host'], port=config['redis']['port'])
 
 while True:
   # get data from opensprinkler
-
+  
   r_logs = requests.get(f'{config["opensprinkler"]["base_uri"]}{ospri_logs}&hist=1')
   r_all  = requests.get(f'{config["opensprinkler"]["base_uri"]}{ospri_all}')
 
@@ -57,9 +77,26 @@ while True:
   water_level = r_all.json()['options']['wl']
   snames		  = r_all.json()['stations']['snames']
   programs 	  = r_all.json()['programs']['pd']
+  status      = r_all.json()['settings']['ps']
+  sbits       = r_all.json()['settings']['sbits']
+  now         = datetime.utcfromtimestamp(r_all.json()['settings']['devt']) # device time needed to calculate total duration based on time left
 
   # map logs to be better processible
-  mapped_logs = list(map(lambda event : {'program': 'manuell' if event[0] == 99 else programs[event[0]-1][5], 'station':snames[event[1]], 'duration':timedelta(seconds=event[2]), 'start':datetime.utcfromtimestamp(event[3] - event[2])}, logs))
+  mapped_logs = list(map(lambda event : {'program': 'manuell' if event[0] == 99 else programs[event[0]-1][5], 'station':snames[event[1]], 'duration':timedelta(seconds=event[2]), 'end':datetime.utcfromtimestamp(event[3])}, logs))
+
+
+  mapped_status = [
+    {
+      'station': snames[i], 
+      'status': { 
+        'running': (1<<i)&sbits[0]>0, # i-th bit is 1 iff station is active
+        'program': 'manuell' if prog == 99 else programs[prog-1][5], 
+        'start':datetime.utcfromtimestamp(start),  
+        'left':timedelta(seconds=int(left)),
+        'duration': now - datetime.utcfromtimestamp(int(start - left))
+      }
+    } for i,[prog,left,start] in enumerate(status)
+  ]
 
   cache.set('wl', water_level)
 
@@ -72,14 +109,28 @@ while True:
 
   # push new log messages only
   for log in mapped_logs:
-    key = 'log:' + log['start'].date().isoformat()
+    key = 'log:' + log['end'].date().isoformat()
     value = json.dumps(log, default=json_serial)
 
     if(cache.sismember(key, value)):
-      print (f'already pushed log message {value}')
+      logging.debug(f'already pushed log message {value}')
     else: 
       logMessage(log, water_level)
       cache.sadd(key, value)
     cache.expire(key, 172800)
+
+  # push new status messages only
+  for status in mapped_status:
+    logging.info('status: ' + json.dumps(status, default=json_serial))
+    key = 'status:' + status['station']
+    #remove time left and duration
+    value = json.dumps({'station':status['station'], 'status':omit(status['status'],('left', 'duration'))}, default=json_serial)
+    if(status['status']['running'] and cache.get(key) != value.encode()):
+      logging.debug (f'pushing status message {value}') 
+      statusMessage(status, water_level)
+      cache.set(key, value)
+    else:
+      logging.debug(f'not pushing status message {value}') 
+      
 
   pause.seconds(config['script']['check_interval'])
